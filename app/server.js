579bc024fc5210c81436167a89c048ceef59b26a
guilleviera96 <guillefce096@gmail.com>
2025-10-09 20:11:13 -0300

    feat/PRO-85 Creadas las entidades de Carritos, CarritosItems, se agrego el atributo contrasena al usuario

--- app/server.js
+++ app/server.js
@@ -1,12 +1,16 @@
 import express from "express";
 import userRoutes from "./routes/userRoutes.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
+import dotenv from 'dotenv'
+dotenv.config()
+const PORT = process.env.PORT || 3000;
+
 
 const app = express();
 app.use(express.json());
 
-app.use("/users", userRoutes);
+// app.use("/users", userRoutes);
 
 app.use(errorHandler);
 
-app.listen(3000, () => console.log("Servidor en http://localhost:3000"));
+app.listen(PORT, () => console.log("Servidor en http://localhost:3000"));
