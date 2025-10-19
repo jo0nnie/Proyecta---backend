74a24c7c107522ab2cb40d7ddcad52df943361e1
FraGal05 <46552826@roquegonzalez.org>
2025-10-19 20:52:12 -0300

    conexion del back para el front

--- app/server.js
+++ app/server.js
@@ -1,13 +1,19 @@
-import { errorHandler } from "./middlewares/errorHandler.js";
+import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
+import { errorHandler } from "./middlewares/errorHandler.js";
+
 dotenv.config({ path: "../.env" });
-const PORT = process.env.PORT || 3000;
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
 
-app.use("/usuarios", userRoutes);
+app.use("/api/usuarios", userRoutes);
 
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`Servidor en http://localhost:${[PORT]}]`));
+app.listen(PORT, () => console.log(`Servidor en http://localhost:${[PORT]}`));
