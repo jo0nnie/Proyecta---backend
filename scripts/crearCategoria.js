ed7117554f8bef071c5edbaf5781c856333f8d4a
guilleviera96 <guillefce096@gmail.com>
2025-10-22 10:48:36 -0300

    Actualizado script para crear categorias

--- scripts/crearCategoria.js
+++ scripts/crearCategoria.js
@@ -1,20 +1,26 @@
 import prisma from "../app/prisma/client.js";
 
 async function main() {
-  const nuevaCategoria = await prisma.categorias.create({
-    data: {
-      nombre: 'Gastronomía',
-    },
-  });
+  const categorias = [
+    { nombre: 'Tecnología' },   // id 1
+    { nombre: 'Arte' },         // id 2
+    { nombre: 'Moda' },         // id 3
+    { nombre: 'Gastronomía' },  // id 4
+  ];
 
-  console.log('Categoría creada:', nuevaCategoria);
+  try {
+    await prisma.categorias.createMany({
+      data: categorias,
+      skipDuplicates: true, // evitar insertar si ya existe
+    });
+
+    console.log('Categoríascreadas:', categorias);
+  } catch (error) {
+    console.error('Error con el insertcategorías:', error);
+    process.exit(1);
+  } finally {
+    await prisma.$disconnect();
+  }
 }
 
-main()
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
-  })
-  .finally(() => {
-    prisma.$disconnect();
-  });
+main();
