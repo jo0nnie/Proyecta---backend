aed99996f2d7b79091902cb5830557d63c437a12
guilleviera96 <guillefce096@gmail.com>
2025-10-24 13:01:37 -0300

    Se modifico el authMiddleware para que el usuario deba estar verificado para realizar ciertas acciones

--- app/middlewares/authMiddleware.js
+++ app/middlewares/authMiddleware.js
@@ -26,6 +26,9 @@ export const authMiddleware = async (req, res, next) => {
     if (!usuario || !usuario.estado) {
       return res.status(403).json({ error: "Sesión cerrada o usuario inactivo" });
     }
+    if (usuario.estado != true){
+      return res.status(403).json({error:'Tu email debe estar verificado para realizar esta accion'})
+    }
 
     req.usuarioId = decoded.id;
     next();
