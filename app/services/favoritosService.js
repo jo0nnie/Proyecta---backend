108ce1a1ca3029f0734af32e5f04e14aebea71a8
FraGal05 <46552826@roquegonzalez.org>
2025-10-30 00:22:28 -0300

    Creados endpoints y rutas para favoritos

---
+++ app/services/favoritosService.js
@@ -0,0 +1,24 @@
+import prisma from "../prisma/client.js";
+
+export const listarFavoritos = async (usuarioId) => {
+  const favoritos = await prisma.favoritos.findMany({
+    where: { usuarioId },
+    include: { emprendimiento: true },
+  });
+  return favoritos;
+};
+
+export const agregarAFavoritos = async (usuarioId, emprendimientoId) => {
+  return await prisma.favoritos.create({
+    data: {
+      usuario: { connect: { id: usuarioId } },
+      emprendimiento: { connect: { id: emprendimientoId } },
+    },
+  });
+};
+
+export const quitarFavorito = async (favoritoId) => {
+  return await prisma.favoritos.delete({
+    where: { id: favoritoId },
+  });
+};
